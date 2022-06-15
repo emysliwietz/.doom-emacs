676de311edc9019d76447c46bc6759bdd2ae2c0b
Egidius Mysliwietz <emysliwietz@github.com>
2022-06-16 00:40:51 +0200

    chapters, subtitles, update resistant ffmpeg binary

--- modules/elfeed-tweaks.el
+++ modules/elfeed-tweaks.el
@@ -362,11 +362,11 @@
       youtube-dl-arguments
       (nconc `("-f" "bestvideo[height<=1080]+bestaudio/best[height<=1080]"
                "--sponsorblock-remove" "default"
-               "-prefer-free-formats"
-               "-embed-subs"
-               "-embed-metadata"
-               "-embed-chapters"
-                                        ;"-ffmpeg-location" "path"
+               "--prefer-free-formats"
+               "--embed-subs"
+               "--embed-metadata"
+               "--embed-chapters"
+               "--ffmpeg-location" "/home/user/.doom.d/ext/bin/"
                "--no-colors")
              youtube-dl-arguments))
                                         ; (setq youtube-dl-arguments nil)
