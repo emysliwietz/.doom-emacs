2ec362753b834ce8f4ade9279d157e118e0b6b4e
egidius <egidius@mysliwietz.de>
2022-09-16 22:44:20 +0200

    Added org tweaks submodule

---
+++ modules/org-tweaks.el
@@ -0,0 +1,17 @@
+;;; org-tweaks.el -*- lexical-binding: t; -*-
+
+(after! org
+  (setq org-directory "~/sync/org/"
+        org-agenda-files "~/sync/agenda/"
+        org-todo-keywords '((sequence "TODO(t)" "LECT(l)" "MEET(m)" "PROJ(p)" "LOOP(r)" "STRT(s)" "WAIT(w)" "HOLD(h)" "IDEA(i)" "|" "DONE(d)" "KILL(k)")
+                            (sequence "[ ](T)" "[-](S)" "[?](W)" "|" "[X](D)")
+                            (sequence "|" "OKAY(o)" "YES(y)" "NO(n)"))
+        org-startup-folded t
+        org-log-done 'time
+        org-log-reschedule 'time)
+
+  ;;; Add org mode to txt and archive files
+  (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+)
+
+(provide 'org-tweaks)
