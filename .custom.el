77635ae715d5fb7a06f06350dd39550125c004fd
egidius <egidius@mysliwietz.de>
2022-09-16 15:18:44 +0200

    More popup defaults

--- .custom.el
+++ .custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(spell-fu all-the-icons-ivy notmuch cmake-mode))
+ '(package-selected-packages
+   '(ivy-youtube spell-fu all-the-icons-ivy notmuch cmake-mode))
  '(warning-suppress-types '((doom-first-buffer-hook) (emacs) (emacs) (defvaralias))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
