2cb6c4e0b3758d24c7f6b2dcef1393d73368ff8d
egidius <egidius@mysliwietz.de>
2023-01-13 22:54:43 +0100

    Merged latex stuff

--- modules/latex-tweaks.el
+++ modules/latex-tweaks.el
@@ -2,3 +2,15 @@
 
 (setq TeX-command-extra-options "--shell-escape")
 (setq TeX-engine 'luatex)
+(add-hook 'org-mode-hook
+          (lambda ()
+            (local-set-key (kbd "<f6>")
+                           (lambda () (interactive) (org-latex-export-to-pdf t)))))
+
+                                        ;(global-set-key [f6] (lambda () (interactive) (org-latex-export-to-pdf t)))
+(setq org-latex-compiler "lualatex")
+(setq-default TeX-master nil)
+(add-to-list 'org-latex-packages-alist
+             '("AUTO" "polyglossia" t ("xelatex" "lualatex")))
+
+(provide 'latex-tweaks)
