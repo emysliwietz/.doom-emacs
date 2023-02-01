b506bfbcef0e159713a9f4868a47776124a27dd6
Egidius <egidius@mysliwietz.de>
2023-02-01 15:19:22 +0100

    Latex Preview should work

--- .custom.el
+++ .custom.el
@@ -3,6 +3,33 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-preview-latex-process-alist
+   '((dvipng :programs
+      ("dvilualatex" "dvipng")
+      :description "dvi > png" :message "you need to install the programs: dvilualatex and dvipng." :image-input-type "dvi" :image-output-type "png" :image-size-adjust
+      (1.0 . 1.0)
+      :latex-compiler
+      ("dvilualatex -interaction nonstopmode -output-directory %o %f")
+      :image-converter
+      ("dvipng -D %D -T tight -o %O %f")
+      :transparent-image-converter
+      ("dvipng -D %D -T tight -bg Transparent -o %O %f"))
+     (dvisvgm :programs
+      ("lualatex" "dvisvgm")
+      :description "dvi > svg" :message "you need to install the programs: lualatex and dvisvgm." :image-input-type "dvi" :image-output-type "svg" :image-size-adjust
+      (1.7 . 1.5)
+      :latex-compiler
+      ("lualatex -interaction nonstopmode -output-directory %o %f")
+      :image-converter
+      ("dvisvgm %f -n -b min -c %S -o %O"))
+     (imagemagick :programs
+      ("lualatex" "convert")
+      :description "pdf > png" :message "you need to install the programs: lualatex and imagemagick." :image-input-type "pdf" :image-output-type "png" :image-size-adjust
+      (1.0 . 1.0)
+      :latex-compiler
+      ("lualatex -interaction nonstopmode -output-directory %o %f")
+      :image-converter
+      ("convert -density %D -trim -antialias %f -quality 100 %O"))))
  '(package-selected-packages
    '(ivy-youtube spell-fu all-the-icons-ivy notmuch cmake-mode))
  '(warning-suppress-types '((doom-first-buffer-hook) (emacs) (emacs) (defvaralias))))
