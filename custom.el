c8d027fcd6b8d24bd9421c07b507451d5a065893
Egidius <egidius@mysliwietz.de>
2023-02-01 15:25:59 +0100

    Latex Preview

--- custom.el
+++ custom.el
@@ -4,11 +4,3 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(warning-suppress-types '((doom-first-buffer-hook) (defvaralias))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(doom-modeline-buffer-modified ((t (:foreground "orange"))))
- '(org-cite ((t (:foreground "DarkSeaGreen4"))))
- '(org-cite-key ((t (:foreground "green" :slant italic)))))
