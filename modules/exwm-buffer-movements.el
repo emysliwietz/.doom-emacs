e77b29a00e7c8179c9b8fd2f7fdd349350c54900
egidius <egidius@mysliwietz.de>
2023-02-13 01:45:16 +0100

    Right is where the thumb is left...

--- modules/exwm-buffer-movements.el
+++ modules/exwm-buffer-movements.el
@@ -1,5 +1,5 @@
-(exwm-input-set-key (kbd "s-h") 'buf-move-right)
-(exwm-input-set-key (kbd "s-l") 'buf-move-left)
+(exwm-input-set-key (kbd "s-h") 'buf-move-left)
+(exwm-input-set-key (kbd "s-l") 'buf-move-right)
 (exwm-input-set-key (kbd "s-k") 'buf-move-up)
 (exwm-input-set-key (kbd "s-j") 'buf-move-down)
 (exwm-input--update-global-prefix-keys)
