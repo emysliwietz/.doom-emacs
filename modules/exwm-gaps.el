ae99ca88c3db3b085e42f7d4e91266a16e8d94db
egidius <egidius@mysliwietz.de>
2023-02-25 23:38:53 +0100

    Used use-package instead of doom version

--- modules/exwm-gaps.el
+++ modules/exwm-gaps.el
@@ -1,7 +1,7 @@
-(use-package exwm-outer-gaps
+(use-package! exwm-outer-gaps
   :after (exwm xelb)
   :config
-  (exwm-outer-gaps-mode +1))
-(exwm-input-set-key (kbd "s-+") 'exwm-outer-gaps-increment)
-(exwm-input-set-key (kbd "s--") 'exwm-outer-gaps-decrement)
+  (exwm-outer-gaps-mode +1)
+  (exwm-input-set-key (kbd "s-+") 'exwm-outer-gaps-increment)
+  (exwm-input-set-key (kbd "s--") 'exwm-outer-gaps-decrement))
 (provide 'exwm-gaps)
