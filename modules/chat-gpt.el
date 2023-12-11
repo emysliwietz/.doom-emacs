e2ae86746a8980bfd14f8eda4b718c6b14af7cc2
Sermak <egidius.mysliwietz@gmx.de>
2023-12-11 19:40:22 +0100

    No more querying API keys at startup for no reason

--- modules/chat-gpt.el
+++ modules/chat-gpt.el
@@ -1,3 +1,7 @@
-(setq gptel-api-key (secrets-get-secret "Passwords" "ChatGPT API Key"))
-(setq gptel--system-message "")
+(use-package! gptel
+  :defer t
+  :config
+    (setq gptel-api-key (secrets-get-secret "Passwords" "ChatGPT API Key")
+       gptel--system-message "")
+  )
 (provide 'chat-gpt)
